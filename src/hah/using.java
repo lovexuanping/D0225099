5943bc68d87f0ddc26d5374ea807a271de75651e
lovexuanping <guanping91@gmail.com>
2015-12-21 09:34:51 +0800

    XD

---
+++ src/hah/using.java
@@ -0,0 +1,26 @@
+package hah;
+
+public class using {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class quetion
+{
+	
+	String des();
+
+
+
+
+
+
+
+}
